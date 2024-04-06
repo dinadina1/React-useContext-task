0fec65d5cd042fc53717dcf6e616314efe3356af
dinadina1 <dinad9355@gmail.com>
2024-04-06 15:53:40 +0530

    React Context API

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -11,23 +11,29 @@ const Cart = () => {
   return (
     <>
       <div className="container">
-        <BacktoHome />
-        <div className='row pt-3 pb-3'>
-          <div className="col-lg-8 col-md-12 overflow-y-auto" style={{ height: "100vh" }}>
-            <ul className="list-group list-group-numbered cart-list">
-              {
-                (cartLists.length) ?
-                  cartLists.map((product) => <CartItem product={product} key={product.id} />)
-                  :
-                  <p className='text-center p-5'>Your Cart is Empty...</p>
-              }
-            </ul>
-          </div>
-          <div className="col-lg-4 col-md-12">
-            <CartSummary />
-          </div>
+        {
+          cartLists.length ? (
+            <>
+              <BacktoHome />
+              <div className='row pt-3 pb-3'>
+                <div className="col-lg-8 col-md-12 overflow-y-auto" style={{ height: "100vh" }}>
+                  <ul className="list-group list-group-numbered cart-list">
+                    {
+                      cartLists.map((product) => <CartItem product={product} key={product.id} />)
+                    }
+                  </ul>
+                </div>
+                <div className="col-lg-4 col-md-12">
+                  <CartSummary />
+                </div>
 
-        </div>
+              </div>
+            </>
+          ) : (
+            <h2 className='text-center p-5'>Your Cart is Empty...</h2>
+          )
+        }
+
       </div>
     </>
   )
